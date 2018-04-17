04ace5b870884c1206771497a4f3ab77c91a958c
Sean <seanstobo@gmail.com>
2018-04-17 12:34:21 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<div class="main-page">
+
+
+
+		<div class="main-page-header">
+			<h1>Simple, powerful & efficient webpages.</h1>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+		</div>
+
+
+		<div class="main-page-hero">
+
+			<div class="main-page-hero-text-box">
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+			</div>
+
+		</div>
+
+		<div class="main-page-services">
+
+		<div class="main-page-services-hero">
+			<div class="main-page-services-title">
+			<h2>Services</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown </p>
+			</div>
+			
+		<div class="main-page-services-content">
+		// icons
+		
+		</div>
+
+
+
+
+
+
+		</div>
+		
+		
+		</div>
+
+
+
+
+		<div class="main-page-page-four"></div>
+		
+		
+		
+		</div>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
